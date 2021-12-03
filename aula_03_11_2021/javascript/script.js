024c1ab5474e558759bbebaa7971e5c3360c242b
Rafael Custodio Silva <rafaelcustodio@msn.com>
2021-12-03 12:52:46 -0200

    Vário Botões JS Site Xuxa
    Aula 03/12/2021

---
+++ aula_03_11_2021/javascript/script.js
@@ -0,0 +1,32 @@
+//Botão 02, tamanho da fonte
+function mudaTamanhoFonte(){
+    document.getElementById("manipula").style.fontSize = "20pt";
+}
+
+//Botão 03, cor da fonte
+function mudaCorFonte(){
+    document.getElementById("manipula").style.color = "blue";
+}
+
+//Botão 04, background
+function mudaBackground(){
+    document.getElementById("manipula").style.background = "rgb(0,0,139)";
+}
+
+//Botão 05, itálico
+function mudaItalico(){
+    document.querySelector("#manipula").style.fontStyle = "italic";
+}
+
+//Botão 06, background
+function mudaNegrito(){
+    document.getElementById("manipula").style.fontWeight = "bold";
+}
+
+//Botão 07, borda
+function mudaBorda(){
+  document.getElementById("manipula").style.border = "solid red 5px"
+
+}
+
+
